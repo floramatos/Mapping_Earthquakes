5b732ec6a79e6d9934b2be6cb5c6b0d06e714023
Flora Matos <floramatos@gmail.com>
2022-01-19 13:42:01 -0800

    Added json file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,60 +3,54 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+  }
+}).addTo(map);
+
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//   }
+
+// }).addTo(map);
+
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
-// Map a single line from LAX to SFO International Airports
-// Coordinates for each point to be used in the line.
-// let line = [
-//     [33.9416, -118.4085],
-//     [37.6213, -122.3790]
-//   ];
-
-// Map multiple lines
-// Coordinates for each point to be used in the polyline.
-// let line = [
-//   [33.9416, -118.4085],
-//   [37.6213, -122.3790],
-//   [40.7899, -111.9791],
-//   [47.4502, -122.3088]
-// ];
-
-// Create a polyline using the line coordinates and make the line red.
-// L.polyline(line, {
-//     color: "yellow"
-//   }).addTo(map);
-
-// Skill Drill: create an airline route from SFO to JFK 
-let map = L.map('mapid').setView([38.35697868483224, -92.63206804067737], 4);
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
-}).addTo(map);;
-
-// with two stops, Austin-Bergstrom(AUS) and Toronto Pearson(YYZ) International Airports
-// Make the route a blue dashed line, with a weight of 4 and opacity of 0.5 on the light map.
-let line = [
-  [37.6213, -122.3790],
-  [30.19005249999633, -97.66772619098839],
-  [43.6782723057728, -79.62187453989213],
-  [40.64129499711439, -73.77804174215507]
-];
-
-L.polyline(line, {
-  color: "blue",
-  weight: 4,
-  opacity: 0.5,
-  dashArray: '5, 5',
-  dashOffset: '0'
 }).addTo(map);
+
